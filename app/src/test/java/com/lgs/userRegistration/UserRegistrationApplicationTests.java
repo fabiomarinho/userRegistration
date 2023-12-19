f57e1f46820dc83ff250e55462b5d872b6260893
Fábio Marinho <fabiomarinho@gmail.com>
2023-12-19 10:39:09 -0500

    Bootstraping project

---
+++ app/src/test/java/com/lgs/userRegistration/UserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgs.userRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
